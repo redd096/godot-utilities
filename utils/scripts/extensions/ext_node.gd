8739bae58d65b8c0ec2b48015765b0bd21ad2b7c
redd096 <martin.babino96@gmail.com>
2025-02-21 16:57:31 +0100

    find ojbects of type extension

--- utils/scripts/extensions/ext_node.gd
+++ utils/scripts/extensions/ext_node.gd
@@ -14,3 +14,14 @@ static func get_components_in_children(node : Node, type : String) -> Array:
 static func get_component_in_children(node : Node, type : String) -> Variant:
 	var components : Array = get_components_in_children(node, type)
 	return components[0] if components.size() > 0 else null
+
+##equivalent of unity FindObjectsOfType<type>
+static func find_objects_of_type(type : String) -> Variant:
+	var root_node : Node = Engine.get_main_loop().current_scene
+	#get components in children but ignore root node
+	return root_node.find_children("*", str(type))
+
+##equivalent of unity FindObjectOfType<type>
+static func find_object_of_type(type : String) -> Variant:
+	var components : Array = find_objects_of_type(type)
+	return components[0] if components.size() > 0 else null
