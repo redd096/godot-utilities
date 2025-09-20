fbdeac9463a438516aa9e2030818ae0ca365f870
redd096 <martin.babino96@gmail.com>
2025-09-20 16:05:04 +0200

    statemachine + update singleton and unity_like to works with type instead of string

--- utils/scripts/static_utilities/unity_like.gd
+++ utils/scripts/static_utilities/unity_like.gd
@@ -1,47 +1,62 @@
 class_name UnityLike
 
-## Equivalent of unity node.GetComponentsInChildren<type>
-static func get_components_in_children(node : Node, type : String) -> Array:
+## Equivalent of unity node.GetComponentsInChildren<script_type>
+static func get_components_in_children(node: Node, script_type: Object) -> Array:
+	var type := _get_string_from_script_type(script_type)
 	var components : Array
-	#add node component
+	# Add node component
 	if node.get_class() == type:
 		components.append(node)
-	#add every child components
+	# Add every child components
 	components.append_array(node.find_children("*", str(type)))
 	return components
 
-## Equivalent of unity node.GetComponentInChildren<type>
-static func get_component_in_children(node : Node, type : String) -> Variant:
-	var components : Array = get_components_in_children(node, type)
+## Equivalent of unity node.GetComponentInChildren<script_type>
+static func get_component_in_children(node : Node, script_type: Object) -> Variant:
+	var components : Array = get_components_in_children(node, script_type)
 	return components[0] if components.size() > 0 else null
 
-## Equivalent of unity FindObjectsOfType<type>
-static func find_objects_of_type(type : String) -> Array:
+## Equivalent of unity FindObjectsOfType<script_type>
+static func find_objects_of_type(script_type: Object) -> Array:
+	var type := _get_string_from_script_type(script_type)
 	var root_node : Node = Engine.get_main_loop().root
-	#get components in children but ignore root node
+	# Get components in children but ignore root node
 	return root_node.find_children("*", str(type), true, false)
 
-## Equivalent of unity FindObjectOfType<type>
-static func find_object_of_type(type : String) -> Variant:
-	var components : Array = find_objects_of_type(type)
+## Equivalent of unity FindObjectOfType<script_type>
+static func find_object_of_type(script_type: Object) -> Variant:
+	var components : Array = find_objects_of_type(script_type)
 	return components[0] if components.size() > 0 else null
 
-## Equivalent of unity DontDestroyOnLoad(node)
-static func dont_destroy_on_load(node : Node) -> void:
-	#check if this is already child of root node
-	var parent : Node = node.get_parent()
+## Equivalent of unity DontDestroyOnLoad(GameObject)
+static func dont_destroy_on_load(node: Node) -> void:
+	## Set root as parent (this isn't destroyed when change scene)
 	var root : Node = Engine.get_main_loop().root
-	if parent and parent == root:
-		return
-	#else remove from current parent
-	if parent:
-		parent.remove_child.call_deferred(node)
-	#and set child of root node (this isn't destroyed when change scene)
-	root.add_child.call_deferred(node)
+	set_parent(node, root)
 
 ## Equivalent of unity node.SetParent(parent)
-static func set_parent(node : Node, parent : Node) -> void:
+static func set_parent(node: Node, parent: Node) -> void:
+	# Check if this is already child of this parent
 	var current_parent = node.get_parent()
+	if current_parent and current_parent == parent:
+		return
+	# Else remove from current parent
 	if current_parent:
 		current_parent.remove_child.call_deferred(node)
+	# And set child of new parent
 	parent.add_child.call_deferred(node)
+
+## Return class name or filename
+static func _get_string_from_script_type(script_type: Object) -> String:
+	var type: String = ""
+	# Check if the argument is a Script resource
+	if script_type is Script:
+		# Get the class name defined by 'class_name'
+		type = script_type.get_global_name()
+		# If no class_name is defined, fall back to the filename
+		if type.is_empty():
+			type = script_type.resource_path.get_file().get_basename()
+	# Otherwise, assume it's a built-in Godot class and use get_class()
+	else:
+		type = script_type.get_class()
+	return type
