c5883bc631e6b6bb967819dd2b5038d0a4ca12e2
redd096 <martin.babino96@gmail.com>
2026-01-04 15:28:53 +0100

    update singleton comments

--- utils/scripts/static_utilities/singleton.gd
+++ utils/scripts/static_utilities/singleton.gd
@@ -7,51 +7,54 @@
 #func _exit_tree():
 	#remove_autoload_singleton("InputHelper")
 
+## Call Singleton.instance(Camera2D) to get Camera2D as it has a static instance variable
 class_name Singleton
 
-## If this is the instance, return true (and can set DontDestroyOnLoad). 
-## If there is another instance, destroy this node and return false. 
-## Normally this functions is called in _ready() for every singleton script
-static func check_instance(self_obj: Node, set_dont_destroy_on_load: bool = true) -> bool:
+## If this is the instance (or there aren't instances and this is set now as instance), return true (and can set DontDestroyOnLoad). 
+## If there is already another instance, destroy this object if destroyCopies is true. 
+## Normally this function is called in _ready() for every singleton script
+static func check_instance(obj: Node, set_dont_destroy_on_load: bool = true, destroy_copies: bool = true) -> bool:
 	# Get current instance or find in scene
-	var script_type = self_obj.get_script()
-	var current_instance : Object = instance(script_type)
-	# If this is the instance, set DontDestroyOnload and return true
-	if current_instance == self_obj:
+	var script_type = obj.get_script()
+	var current_instance: Object = instance(script_type)
+	# If this is the instance, return true
+	if current_instance == obj:
+		# And set DontDestroyOnLoad
 		if set_dont_destroy_on_load:
-			_dont_destroy_on_load(self_obj)
+			_dont_destroy_on_load(obj)
 		return true
 	# Else, destroy and return false
 	else:
-		self_obj.queue_free()
+		if destroy_copies:
+			obj.queue_free()
 		return false
 
-## Return registered instance. If not registered, find in scene or instantiate it. 
+## Return instance for this type. If there isn't, find in scene or instantiate it
 ## e.g. Singleton.instance(Camera2D, true) to look for Camera2D in scene, else instantiate it
 ## e.g. Singleton.instance(Camera2D, false) to look for Camera2D in scene, else return null
 static func instance(script_type: Object, auto_instantiate: bool = false) -> Variant:
-	# If singleton is already registered and it's still valid, return it
+	# If there is an instance and it's still valid, return it
 	var type := _get_string_from_script_type(script_type)
 	if Engine.has_singleton(type):
 		var current_instance = Engine.get_singleton(type)
 		if current_instance:
 			return current_instance
-	# If singleton isn't already registered or it is no more valid
-	# try find it in scene
-	var obj_instance : Object = _find_object_of_type(script_type)
+	# If there isn't an instance ot it's not valid, try find in scene
+	var obj_instance: Object = _find_object_of_type(script_type)
 	# or instantiate it
 	if obj_instance == null and auto_instantiate:
-		print(type, " is null. It will be automatically instantiated")
 		obj_instance = script_type.new()
 		obj_instance.name = str(type, " (Auto Instantiated)")
-	# Register it
+	# Then register it
 	if obj_instance:
 		Engine.register_singleton(type, obj_instance)
 	return obj_instance
 
+#region copy-paste from unity_like
+
 ## Return class name or filename
 static func _get_string_from_script_type(script_type: Object) -> String:
-	var type : String = ""
+	var type: String = ""
 	# Check if the argument is a Script resource
 	if script_type is Script:
 		# Get the class name defined by 'class_name'
@@ -67,20 +70,23 @@ static func _get_string_from_script_type(script_type: Object) -> String:
 ## Equivalent of unity FindObjectOfType<type>
 static func _find_object_of_type(script_type: Object) -> Variant:
 	var type := _get_string_from_script_type(script_type)
-	var root_node : Node = Engine.get_main_loop().root
+	var root_node: Node = Engine.get_main_loop().root
 	# Get components in children but ignore root node
-	var components : Array = root_node.find_children("*", str(type), true, false)
+	var components: Array = root_node.find_children("*", str(type), true, false)
 	return components[0] if components.size() > 0 else null
 
 ## Equivalent of unity DontDestroyOnLoad(GameObject)
-static func _dont_destroy_on_load(node : Node) -> void:
-	# Check if this is already child of root node
-	var parent : Node = node.get_parent()
-	var root : Node = Engine.get_main_loop().root
-	if parent and parent == root:
+static func _dont_destroy_on_load(node: Node) -> void:
+	# Set root as parent (this isn't destroyed when change scene)
+	var root: Node = Engine.get_main_loop().root
+	# Check if this is already child of this parent
+	var current_parent = node.get_parent()
+	if current_parent and current_parent == root:
 		return
 	# Else remove from current parent
-	if parent:
-		parent.remove_child.call_deferred(node)
-	# And set child of root node (this isn't destroyed when change scene)
+	if current_parent:
+		current_parent.remove_child.call_deferred(node)
+	# And set child of new parent
 	root.add_child.call_deferred(node)
+
+#endregion
--- utils/scripts/static_utilities/unity_like.gd
+++ utils/scripts/static_utilities/unity_like.gd
@@ -3,7 +3,7 @@ class_name UnityLike
 ## Equivalent of unity node.GetComponentsInChildren<script_type>
 static func get_components_in_children(node: Node, script_type: Object) -> Array:
 	var type := _get_string_from_script_type(script_type)
-	var components : Array
+	var components: Array
 	# Add node component
 	if node.get_class() == type:
 		components.append(node)
@@ -12,26 +12,26 @@ static func get_components_in_children(node: Node, script_type: Object) -> Array
 	return components
 
 ## Equivalent of unity node.GetComponentInChildren<script_type>
-static func get_component_in_children(node : Node, script_type: Object) -> Variant:
-	var components : Array = get_components_in_children(node, script_type)
+static func get_component_in_children(node: Node, script_type: Object) -> Variant:
+	var components: Array = get_components_in_children(node, script_type)
 	return components[0] if components.size() > 0 else null
 
 ## Equivalent of unity FindObjectsOfType<script_type>
 static func find_objects_of_type(script_type: Object) -> Array:
 	var type := _get_string_from_script_type(script_type)
-	var root_node : Node = Engine.get_main_loop().root
+	var root_node: Node = Engine.get_main_loop().root
 	# Get components in children but ignore root node
 	return root_node.find_children("*", str(type), true, false)
 
 ## Equivalent of unity FindObjectOfType<script_type>
 static func find_object_of_type(script_type: Object) -> Variant:
-	var components : Array = find_objects_of_type(script_type)
+	var components: Array = find_objects_of_type(script_type)
 	return components[0] if components.size() > 0 else null
 
 ## Equivalent of unity DontDestroyOnLoad(GameObject)
 static func dont_destroy_on_load(node: Node) -> void:
-	## Set root as parent (this isn't destroyed when change scene)
-	var root : Node = Engine.get_main_loop().root
+	# Set root as parent (this isn't destroyed when change scene)
+	var root: Node = Engine.get_main_loop().root
 	set_parent(node, root)
 
 ## Equivalent of unity node.SetParent(parent)
