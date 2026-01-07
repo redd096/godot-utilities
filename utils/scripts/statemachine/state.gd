9a528a31a5ef422f4ed276b8932d88482c93f47e
redd096 <martin.babino96@gmail.com>
2026-01-07 16:32:43 +0100

    disable process in State

--- utils/scripts/statemachine/state.gd
+++ utils/scripts/statemachine/state.gd
@@ -3,6 +3,10 @@ class_name State extends Node
 
 var state_machine: StateMachine
 
+func _ready() -> void:
+	# disable process_mode, because we'll use _process_state called by state_machine
+	process_mode = PROCESS_MODE_DISABLED
+
 func _enter_state() -> void:
 	pass
 
