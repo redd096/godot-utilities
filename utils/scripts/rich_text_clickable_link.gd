cb31c8dc2014d0bdeb7d010cfa360caa3eabd779
redd096 <martin.babino96@gmail.com>
2026-03-06 00:02:38 +0100

    script to click rich text links

---
+++ utils/scripts/rich_text_clickable_link.gd
@@ -0,0 +1,9 @@
+class_name RichTextClickableLink extends RichTextLabel
+
+func _ready():
+	# Connect the `meta_clicked` signal to the `_richtextlabel_on_meta_clicked` method
+	meta_clicked.connect(_richtextlabel_on_meta_clicked)
+
+func _richtextlabel_on_meta_clicked(meta):
+	# `meta` is of Variant type, so convert it to a String to avoid script errors at run-time.
+	OS.shell_open(str(meta))
